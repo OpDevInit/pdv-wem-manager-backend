730a8e456b55d792c83d912811d4185a63784ad6
Jhonata Ferreira <101606104+OpDevInit@users.noreply.github.com>
2026-06-13 01:07:33 -0300

    Update ProdutoRepository.java

--- wem-sc-vendas/src/main/java/com/wemsellers/wem_sc_vendas/repository/ProdutoRepository.java
+++ wem-sc-vendas/src/main/java/com/wemsellers/wem_sc_vendas/repository/ProdutoRepository.java
@@ -1,6 +1,7 @@
 package com.wemsellers.wem_sc_vendas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wemsellers.wem_sc_vendas.model.Produto;
 
