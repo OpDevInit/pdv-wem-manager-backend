9abdb568a4c23a43af69bee440652dabc350823c
Jhonata Ferreira <101606104+OpDevInit@users.noreply.github.com>
2026-06-13 00:49:29 -0300

    Update Produto.java

--- wem-sc-vendas/src/main/java/com/wemsellers/wem_sc_vendas/model/Produto.java
+++ wem-sc-vendas/src/main/java/com/wemsellers/wem_sc_vendas/model/Produto.java
@@ -6,7 +6,11 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Produto {
+    
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     private String codigoBarras;
     private String nome;
     private double preco;
